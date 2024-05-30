426c8243d7bc0465232d4033d5964a2a8be1eb87
Canon <canon.heise@gmail.com>
2024-05-30 11:37:13 -0600

    week 6 code

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography.X509Certificates;
+
 public class Node {
     public int Data { get; set; }
     public Node? Right { get; private set; }
@@ -8,29 +10,59 @@ public class Node {
     }
 
     public void Insert(int value) {
-        if (value < Data) {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
-            else
-                Left.Insert(value);
-        }
-        else {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
+        if (Data != value){
+            if (value < Data) {
+                // Insert to the left
+                if (Left is null)
+                    Left = new Node(value);
+                else
+                    Left.Insert(value);
+            }
+            else {
+                // Insert to the right
+                if (Right is null)
+                    Right = new Node(value);
+                else
+                    Right.Insert(value);
+            }
         }
     }
 
     public bool Contains(int value) {
         // TODO Start Problem 2
-        return false;
+        if (Data != value){
+            if (value < Data) {
+                if (Left is null)
+                    return false;
+                else
+                    return Left.Contains(value);
+            }
+            else {
+                if (Right is null)
+                    return false;
+                else
+                    return Right.Contains(value);
+            }
+        }
+        return true;
     }
 
     public int GetHeight() {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        var x = 0;
+        var y = 0;
+        if (Right is not null){
+            x = Right.GetHeight();
+        }
+        if (Left is not null){
+            y = Left.GetHeight();
+        }
+        if (x > y){
+            return x + 1;
+        }
+        else{
+            return y + 1;
+        }
+        
     }
 }
